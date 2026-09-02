6ee906087ab8721192e10138f4cbfacfb8a92e3a
Chen Hanchuan <hchen628@connect.hkust-gz.edu.cn>
2026-09-02 18:39:42 +0800

    feat: implement Monte Carlo pi estimator

---
+++ week1/pi.py
@@ -0,0 +1,14 @@
+import random
+
+
+def estimate_pi(n, seed):
+    rng = random.Random(seed)
+    inside_circle = 0
+
+    for _ in range(n):
+        x = rng.random()
+        y = rng.random()
+        if x * x + y * y <= 1:
+            inside_circle += 1
+
+    return 4 * inside_circle / n
