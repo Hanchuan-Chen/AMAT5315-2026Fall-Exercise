99dba3671e8d104488d8be0db14a94b1f6f33119
Chen Hanchuan <hchen628@connect.hkust-gz.edu.cn>
2026-09-10 12:46:43 +0800

    feat: write raw Metropolis temperature sweep

--- week3/src/lib.rs
+++ week3/src/lib.rs
@@ -4,7 +4,12 @@ pub mod cli;
 mod lattice;
 mod metropolis;
 mod snapshots;
+mod temperature_sweep;
 
 pub use lattice::Lattice;
 pub use metropolis::{AcceptanceTable, RelaxConfig, RelaxResult, SweepStats, relax, sweep};
 pub use snapshots::{SnapshotConfig, SnapshotSummary, write_snapshots};
+pub use temperature_sweep::{
+    TemperatureSweepConfig, TemperatureSweepSummary, course_temperature_grid,
+    write_temperature_sweep,
+};
